4103ea4c7d8ad3132c90981d0bad834e8b68a669
Vraj Shrimali <101273123+vrajs-data@users.noreply.github.com>
2026-06-13 08:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def home():
 
 @app.route('/projects')
 def projects():
-    return render_template('projects.html')
+    # return render_template('projects.html')
+    return "Projects page works!"
 
 @app.route('/test')
 def test():
